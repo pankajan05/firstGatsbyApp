266b014117e0517b7d672a84d0fc4453f615fa48
Pankajan Satkunam <pankajan05@gmail.com>
2020-02-23 17:31:48 +0530

    add the content

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,8 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people,</h1>
     <p>Welcome to my new Gatsby site.</p>
+    <p>I'm pankajan satkunam. I am Software Engineer Undergraduate student and University
+     of Kelaniya. I am an full stack webdeveloper. I like to learn new technologies that is why i build this site.</p>
     <p>Here I am built this website for learn the Gatsby framework. If you like to create like this then you can contact me through the link.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />

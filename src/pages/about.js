093400dcda16b0e8ab165da4f7423c2978b893f6
Pankajan Satkunam <pankajan05@gmail.com>
2020-02-23 17:58:44 +0530

    Add the content to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,16 @@ const AboutPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>About me</h1>
+    <p>I am currently study in university of kelaniya. I am interested in learning new things. I
+    like Information technology field very much. I am currently learning in web development.
+    I have desire to learn cloud computering, Artificial intelligence and Data science.</p>
+    <p>
+      Rather than leaning i am interested with the nature. I love explore the new places and nature.
+      most of my semester vacations, I went to hiking and camping with my friends.It give me mind relasing to me.
+    </p>
+      <p>I do gardening in my home in my free time. I like to grow flowers and vegetables plants
+        in my garden.
+    </p>
   </Layout>
 )
 
